e160ba34b53b05639013266667623fac1eb2f5b9
Ahmet Arsan <aarsan@progger.com>
2022-01-19 11:24:54 -0600

    moved config from env to config.json

--- azure-speech-streaming-reactjs/speechexpressbackend/serverapp.js
+++ azure-speech-streaming-reactjs/speechexpressbackend/serverapp.js
@@ -2,7 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const axios = require('axios');
 const app = express()
-const port = process.env.WEB_PORT || 8080; //Don't use port 3000 since React using port 3000 by default
+
+// get config
+const config = require('./config.json')
+const port = config[0].web_port
+const speechKey = config[0].subscription_key;
+const speechRegion = config[0].region;
+const endpoint_id = config[0].endpoint_id;
+const textAnalyticsKey = config[0].text_analytics_key;
+const textAnalyticsEndpoint = config[0].text_analytics_endpoint;    
+
 
 //"use strict";
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
@@ -16,8 +25,7 @@ app.get('/api/sayhello', (req, res) => {
 
 app.get('/api/get-speech-token', async (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
-    const speechKey = process.env.SPEECH_KEY;
-    const speechRegion = process.env.SPEECH_REGION;
+
 
     if (speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
         res.status(400).send('You forgot to add your speech key or region to the .env file.');
@@ -30,9 +38,9 @@ app.get('/api/get-speech-token', async (req, res, next) => {
         };
 
         try {
-            console.log(`Speechkey loaded for speech region ${speechRegion}. Gettig token`)
+            console.log(`Speechkey loaded for speech region ${speechRegion}. Getting token`)
             const tokenResponse = await axios.post(`https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, null, headers);
-            res.send({ token: tokenResponse.data, region: speechRegion });
+            res.send({ token: tokenResponse.data, region: speechRegion, endpoint_id: endpoint_id });
         } catch (err) {
             res.status(401).send('There was an error authorizing your speech key.');
         }
@@ -40,10 +48,6 @@ app.get('/api/get-speech-token', async (req, res, next) => {
 });
 
 app.post('/api/ta-key-phrases', async (req, res) => { 
-    //You can find your key and endpoint in the resource's key and endpoint page, under resource management.
-    const textAnalyticsKey = process.env.TEXTANALYTICS_KEY;
-    const textAnalyticsEndpoint = process.env.TEXTANALYTICS_ENDPOINT;    
-
     const requestJSON = JSON.stringify(req.body);
     //console.log('JSON string request body ' + requestJSON);
 
@@ -97,7 +101,6 @@ app.post('/api/ta-key-phrases', async (req, res) => {
             }
         }
 
-
         const headers = { 'Content-Type': 'application/json' };  
         res.headers = headers;                  
         //res.send({ keyPhrasesExtracted: keyPhraseResponse, entityExtracted: entityResults, piiExtracted: piiResults });
@@ -110,8 +113,8 @@ app.post('/api/ta-key-phrases', async (req, res) => {
 
 app.post('/api/ta-key-phrases-old', async (req, res) => { 
     //You can find your key and endpoint in the resource's key and endpoint page, under resource management.
-    const textAnalyticsKey = process.env.TEXTANALYTICS_KEY;
-    const textAnalyticsEndpoint = process.env.TEXTANALYTICS_ENDPOINT;  
+    const textAnalyticsKey = config[0].text_analytics_key;
+    const textAnalyticsEndpoint = config[0].text_analytics_endpoint;  
     const requestJSON = JSON.stringify(req.body);
     //console.log('JSON string request body ' + requestJSON);
     const requestText = JSON.stringify(req.body.transcript);
